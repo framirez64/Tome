94436a0ee048606f488f132b24d1dfa7d6d5fcae
tlaloc69 <142059205+framirez64@users.noreply.github.com>
2026-04-01 13:10:55 -0500

    Defining types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,1 +1,20 @@
-    
+export interface Page{
+    pageNumber: number;
+    text: string;
+    offset: number;
+}
+
+export interface DocumentMetadata{
+    uploadAt: Date;
+    wordCount: number;
+    pageCount: number;
+    fileSize: number;
+}
+export interface Document{
+    id: string;
+    filename: string;
+    fileType: 'txt' | 'pdf';
+    fullText: string;
+    metadata: DocumentMetadata;
+    pages: Page[];
+}
